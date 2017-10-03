17f84422baef3c656941adb0b7fbe3f8763aef07
MrsPepa <jose.leon1996@gmail.com>
2017-10-03 09:50:18 -0300

    Subiendo ejercicios funciones matematicas

---
+++ app.js
@@ -0,0 +1,17 @@
+var add = function(num1 , num2){
+  return num1+num2;
+}
+var multiply = function(mult1, mult2){
+  return mult1*mult2;
+}
+console.log(multiply(36325,add(9824,777)));
+
+/*otra opcion
+function add(a,b){
+return a+b;
+}
+function multiply (c,d){
+return c*d;
+}
+en la consola reemplazar los numeros de la funcion
+console.log(multiply(36325,add(9824,777));*/
